fc440575764271e2533330dd66c74db5a45c5ac3
Frank Celler <f.celler@triagens.de>
2015-05-02 19:18:49 +0200

    added webui

--- mesos/framework.sh
+++ mesos/framework.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+if test -z "$ARANGODB_WEBUI";  then
+  if test "$ARANGODB_WEBUI_USE_HOST" = "yes";  then
+    ARANGODB_WEBUI=http://${HOST}:${PORT0}/
+  else
+    ARANGODB_WEBUI=http://${HOSTNAME}:${PORT0}/
+  fi
+fi
+
 env
 
-# exec /mesos/arangodb-framework --http-port=8181 --webui=http://${HOST}:8181/ "$@"
-exec /mesos/arangodb-framework --http-port=${PORT0} --webui=http://${HOST}:${PORT0}/ "$@"
+exec /mesos/arangodb-framework "--http-port=${PORT0}" "--webui=${ARANGODB_WEBUI}" "$@"
